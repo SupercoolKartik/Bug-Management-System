bdcf5e4c96032848a025eb998dcf3c21c39ffc23
Suraj <surajkartik71@gmail.com>
2023-12-29 22:22:48 +0530

    1.Added functionalities to create_project page to take data through text/checkbox input and send the data to the server, 2.Modified '/afterProjectCreation' route so that it can take the datas recieved from create_project page and put them into projects and users_projects tables and then redirects to the /projects page, 3.Modiefied the projects page so it can show all the projects related to currently logged in user, 4.Also removed unnecessary syntaxes from the routes files

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,27 @@
 import express from "express";
+import { promisify } from "util";
 import fs from "fs/promises";
 import pool from "../pool.mjs";
 import ejs from "ejs";
 import { uid } from "uid";
 
+const getConnectionAsync = promisify(pool.getConnection).bind(pool);
+const queryAsync = promisify(pool.query).bind(pool);
+
 const router = express.Router();
 
-let nm = "Suraj";
+//User Id of currently logged in user
+let uIdCurr = "0e6ae53a364";
 
 router.get("/", (req, res) => {
   res.render("login", { err_mess: "" });
 });
 router.post("/afterLogin", (req, res) => {
+  uIdCurr = req.body.uId;
+
   pool.getConnection((err, connection) => {
     if (err) throw err;
+
     connection.query("SELECT uId,uPassword FROM users", (err, result) => {
       if (err) throw err;
       console.log(result);
@@ -24,6 +32,7 @@ router.post("/afterLogin", (req, res) => {
           if (req.body.password == e.uPassword) {
             res.render("main");
             connection.release();
+            console.log("2------>", uIdCurr);
             return;
           }
         }
@@ -47,6 +56,7 @@ router.post("/afterSigningUp", (req, res) => {
     });
     return;
   }
+
   const uId = uid();
   const values = [
     uId,
@@ -69,7 +79,6 @@ router.post("/afterSigningUp", (req, res) => {
       if (err) throw err;
 
       res.render("welcome", { userId: uId });
-      // Release the connection back to the pool
       connection.release();
     });
   });
@@ -85,57 +94,97 @@ router.get("/employees", (req, res) => {
   });
 });
 router.get("/projects", (req, res) => {
-  res.render("projects");
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query(
+      `SELECT pId, pName FROM users_projects WHERE uId = ?`,
+      uIdCurr,
+      (err, result) => {
+        if (err) throw err;
+
+        res.render("projects", { projects: result });
+        connection.release();
+        return;
+      }
+    );
+  });
 });
 router.get("/create_project", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
     connection.query("SELECT uId,fName FROM users", (err, result) => {
       if (err) throw err;
+
       res.render("create_project", { employees: result });
       connection.release();
     });
   });
 });
 
-//-------------------------------------------------
 router.post("/afterProjectCreation", (req, res) => {
-  //const emp = req.body.selectedEmployees || [];
-  console.log(req.body);
+  const emp = req.body.selectedEmployees || [];
+  const project_name = req.body.proj_name || [];
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+
+    let projectId = uid(6);
+
+    //Adding to projects table
+    const sqlP = `INSERT INTO projects (pId,pName,uIdCb) VALUES(?,?,?)`;
+    const valuesP = [projectId, project_name, uIdCurr];
+    connection.query(sqlP, valuesP, (err, result) => {
+      if (err) {
+        connection.release();
+        res.status(500).send(err.message);
+        return;
+      }
+
+      //Adding to users_project table
+      const sqlUp = `INSERT INTO users_projects (id,uId,pId,pName) VALUES(?,?,?,?)`;
+
+      emp.forEach((emp_name) => {
+        const userProjectId = uid(4);
+        let valuesUp = [userProjectId, emp_name, projectId, project_name];
+        connection.query(sqlUp, valuesUp, (err, result) => {
+          if (err) {
+            res.status(500).send(err.message);
+            return;
+          }
+        });
+      });
+
+      connection.query(
+        "SELECT uId,pName FROM users_projects",
+        (err, result) => {
+          if (err) {
+            res.status(500).send(err.message);
+            return;
+          }
+          console.log("The users_projects table", result);
+          return;
+        }
+      );
+      connection.query("SELECT uIdCb,pName FROM projects", (err, result) => {
+        if (err) {
+          res.status(500).send(err.message);
+          return;
+        }
+        console.log("The projects table", result);
+        return;
+      });
+
+      connection.release();
+      return res.redirect("/projects");
+    });
+  });
 });
+
 router.get("/project", (req, res) => {
   res.render("project");
 });
-//------------------------------
-
-// router.post("/", (req, res) => {
-//   // const abc = {
-//   //   name: req.body.name,
-//   //   description: req.body.password,
-//   // };
-//   //res.render("home", req.body.name);
-//   nm = req.body.name + req.body.password;
-//   res.redirect("/");
-// });
-// //-----------------------------
-// router.get("/dbData", (req, res) => {
-//   pool.getConnection((err, connection) => {
-//     if (err) throw err;
-//     connection.query(
-//       `INSERT INTO users01 (id, username, email) VALUES (1,"suraj","s@abc")`,
-//       (err, result) => {
-//         if (err) {
-//           res.send(err);
-//         }
-//       }
-//     );
-//     connection.query("SELECT * FROM users01", (err, result) => {
-//       if (err) throw err;
-//       res.send(result);
-//       // Release the connection back to the pool
-//       connection.release();
-//     });
-//   });
-// });
 
 export default router;
